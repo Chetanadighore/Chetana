499c72298a5cebdace568d6f2ed8d418b9b50d7c
unknown <chetanadighore0621@gmail.com>
2022-07-08 19:57:57 +0530

    Second commit

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 class Test
 {
-    public static void main(string a[]){
-        system.out.println(x:"Hello World");
+    public static void main(string ar[]){
+
+        int a=10, b=20, d=30;
+        int c=a+b;
+        system.out.println("Result"+c);
+
     }
 }
